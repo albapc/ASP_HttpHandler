3ca53dd08a5e94042092301e40ee8ae7e48e13c5
Alba Pérez César <alba.perez.ce@gmail.com>
2021-02-25 11:56:00 +0100

    commit final

---
+++ JpegHandler/App_Code/JpegHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JpegHandler.App_Code
+{
+    // Para evitar el Error 500.23 al ejecutar:
+    // Cambiar el pipeline a modo clásico
+    // Ver: https://stackoverflow.com/questions/5914744/force-iis-express-to-classic-pipeline-mode
+
+
+    // URLs a ejecutar: 
+    /* Para obtener la img del koala:
+     * localhost:XXXX/Images/koala.jpg
+     * 
+     * Para obtener la img de la medusa:
+     * localhost:XXXX/Images/jellyfish.jpg
+     * 
+     * Para lanzar el "not found":
+     * localhost:XXXX/Images/XXXX.jpg (imagen que no esté almacenada en la carpeta Images)
+     */
+
+    public class JpegHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            // set the MIME type
+            context.Response.ContentType = "image/jpeg";
+
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            if(request.RawUrl.ToLower().Contains("jellyfish"))
+            {
+                response.TransmitFile(request.PhysicalApplicationPath + "/Images/Jellyfish.jpg");
+            }
+            else if (request.RawUrl.ToLower().Contains("koala"))
+            {
+                response.TransmitFile(request.PhysicalApplicationPath + "/Images/Koala.jpg");
+            }
+            else
+            {
+                response.Write("File not found");
+            }
+
+
+        }
+    }
+}
+ 
